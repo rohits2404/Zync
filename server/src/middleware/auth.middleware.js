d3036d46d73048c94c4a24b60bc17e6fefa5799f
Rohit Sharma <rohits8731@gmail.com>
2026-03-10 19:19:06 +0530

    02: Server

--- server/src/middleware/auth.middleware.js
+++ server/src/middleware/auth.middleware.js
@@ -1,7 +1,14 @@
+import { getAuth } from "@clerk/express";
+
 export const protectRoute = (req, res, next) => {
-    const { userId } = req.auth;
+    const { userId } = getAuth(req);
+
     if (!userId) {
-        return res.status(401).json({ message: "Unauthorized - You Must Be Logged In" });
+        return res.status(401).json({
+            message: "Unauthorized - You Must Be Logged In",
+        });
     }
+
+    req.auth = { userId }; // attach it so controllers can use it
     next();
 };
